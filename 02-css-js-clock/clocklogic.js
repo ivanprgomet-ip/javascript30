00dd66f2911a8c2d3292977690ec26abbdcc2c7e
Ivan Prgomet <ivan_prgomet@hotmail.com>
2016-12-15 18:16:19 +0100

    small bugfix

--- 02-css-js-clock/clocklogic.js
+++ 02-css-js-clock/clocklogic.js
@@ -12,22 +12,29 @@ var HourHand = document.querySelector("#hour-hand");
 
             //getting current second
             var currentSecond = now.getSeconds(); //sample: 20,21,22,23,24 etc.
-            var degreesPerSecond = ((currentSecond / 60) * 360)+90;
+            var degreesPerSecond = ((currentSecond / 60) * 360)+90;//why +90? to offset hand to the default degree 
             secondHand.style.transform = `rotate(${degreesPerSecond}deg)`;
 
             var currentMinute = now.getMinutes();//sample: 5 (60s later), 6 (60s later), 7... etc.
-            var degreesPerMinute = ((currentMinute/60)*360)+90;
+            var degreesPerMinute = ((currentMinute/60)*360)+90;//60/60=1 * 360 
             minuteHand.style.transform = `rotate(${degreesPerMinute}deg)`;
 
             var currentHour = now.getHours(); //sample: 16 (60min later),17 (60 min later).. etc.
-            var degreesPerHour = ((currentHour/60)*360)+90;
+            var degreesPerHour = ((currentHour/12)*360)+90;
             HourHand.style.transform = `rotate(${degreesPerHour}deg)`;
 
             //setting the transition property to none when we tick into 60th second 
             //to avoid the 'bug' when the hands get reset back to their intiiali position
             //and then right into current date positions 
-            if(currentSecond == 0)
+            if(currentSecond == 0){
                 secondHand.style.transition = "none";
-            else
-                secondHand.style.transition = "all 0.2s cubic-bezier(0.34,-1.15, 0, 1.98)";
+                minuteHand.style.transition = "none";
+                HourHand.style.transition = "none";
+
+            }
+            else{
+                secondHand.style.transition = "all 0.05s cubic-bezier(0.34,-1.15, 0, 1.98)";
+                minuteHand.style.transition = "all 0.05s cubic-bezier(0.34,-1.15, 0, 1.98)";
+                HourHand.style.transition = "all 0.05s cubic-bezier(0.34,-1.15, 0, 1.98)";
+            }
         }
